b181a0d03e1bfab8a5528496526a69f202320b18
Tatyana.Popovskaya <ssttaa87@yandex.ru>
2022-06-25 21:45:32 +0700

    Commit more Tests

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -27,4 +27,76 @@ public class BonusServiceTest {
 
         assertEquals(expected, actual);
     }
+    @org.junit.jupiter.api.Test
+    void shouldCalculateForRegisteredAndZeroValue() {
+        BonusService service = new BonusService();
+
+        long amount = 0;
+        boolean registered = true;
+        long expected = 0;
+
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+    }
+    @org.junit.jupiter.api.Test
+    void shouldCalculateForRegisteredAndNegativeValue() {
+        BonusService service = new BonusService();
+
+        long amount = -1_000;
+        boolean registered = true;
+        long expected = -30;
+
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+    }
+    @org.junit.jupiter.api.Test
+    void shouldCalculateForUnregisteredAndUnderLimit() {
+        BonusService service = new BonusService();
+
+        long amount = 1_000;
+        boolean registered = false;
+        long expected = 10;
+
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+    }
+    @org.junit.jupiter.api.Test
+    void shouldCalculateForUnregisteredAndOverLimit() {
+        BonusService service = new BonusService();
+
+        long amount = 1_000_000;
+        boolean registered = false;
+        long expected = 500;
+
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+    }
+    @org.junit.jupiter.api.Test
+    void shouldCalculateForUnregisteredAndZeroValue() {
+        BonusService service = new BonusService();
+
+        long amount = 0;
+        boolean registered = false;
+        long expected = 0;
+
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+    }
+    @org.junit.jupiter.api.Test
+    void shouldCalculateForUnregisteredAndNegativeValue() {
+        BonusService service = new BonusService();
+
+        long amount = -1_000;
+        boolean registered = false;
+        long expected = -10;
+
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+    }
 }
